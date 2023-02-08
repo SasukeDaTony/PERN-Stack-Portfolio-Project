bf6a899e07800ee2198d1dc1fc77301f4b4ed9b4
SasukeDaTony <tonytaylor@pursuit.org>
2023-02-07 22:45:08 -0500

    app.js treatmentsController start

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+//DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+const allTreatments = require("./controllers/treatmentsController");
+
+//CONFIGURATION
+const app = express();
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use("/treatments", allTreatments);
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("WELCOME TO TONY'S BODY SHOP!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Found Error:404");
+});
+
+module.exports = app;
