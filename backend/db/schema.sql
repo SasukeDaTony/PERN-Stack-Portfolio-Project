f4181efe7b53a4079a905f290bc608fcd3992cac
SasukeDaTony <tonytaylor@pursuit.org>
2023-02-08 12:46:17 -0500

    some schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,2 +1,17 @@
 DROP DATABASE IF EXISTS treatments_db;
 CREATE DATABASE treatments_db;
+
+\c treatments_db;
+
+DROP TABLE IF EXISTS treatments; 
+
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    treatment_image TEXT,
+    category TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL DEFAULT 0,
+    therapist NOT NULL VARCHAR(255),
+    therapist_image TEXT,
+)
